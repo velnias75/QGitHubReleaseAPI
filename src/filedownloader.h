ce5a9b38d2eec1c47164eb6b7d0194c6c25b9212
velnias75 <heiko@rangun.de>
2015-07-04 13:41:54 +0200

    fixed file headers

--- src/filedownloader.h
+++ src/filedownloader.h
@@ -1,20 +1,20 @@
 /*
  * Copyright 2015 by Heiko Schäfer <heiko@rangun.de>
  *
- * This file is part of NetMauMau Qt Client.
+ * This file is part of QGitHubReleaseAPI.
  *
- * NetMauMau Qt Client is free software: you can redistribute it and/or modify
+ * QGitHubReleaseAPI is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
  *
- * NetMauMau Qt Client is distributed in the hope that it will be useful,
+ * QGitHubReleaseAPI is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with NetMauMau Qt Client.  If not, see <http://www.gnu.org/licenses/>.
+ * along with QGitHubReleaseAPI.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef FILEDOWNLOADER_H
